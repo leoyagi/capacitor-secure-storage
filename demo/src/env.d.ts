33d252d305a8e163ac45a56bc3ed90c69cb675b6
Leo Yagi <leoyagi.dev@gmail.com>
2026-01-06 18:48:03 +0100

    simplify

--- demo/src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_SECURE_STORAGE_ENCRYPTION_KEY: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
