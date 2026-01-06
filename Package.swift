6ce14c8616f03bbbf1ec6abf090fcd2705dfb241
Leo Yagi <leoyagi.dev@gmail.com>
2026-01-06 16:00:35 +0100

    fix dependencies

--- Package.swift
+++ Package.swift
@@ -11,13 +11,15 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+        .package( url: "https://github.com/evgenyneu/keychain-swift.git", from: "21.0.0")
     ],
     targets: [
         .target(
             name: "AparajitaCapacitorSecureStorage",
             dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm")
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "KeychainSwift", package: "keychain-swift")
             ],
             path: "ios/Sources/SecureStoragePlugin"
         ),
